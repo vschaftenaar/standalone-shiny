1801bf55609d3676199ba2bc38660902ca2e56f1
Abulily <4079204+vschaftenaar@users.noreply.github.com>
2022-07-02 18:48:25 +0200

    Update run_app.R

--- src/r/run_app.R
+++ src/r/run_app.R
@@ -4,15 +4,17 @@ library(shiny)
 setwd('../../')
 
 file.path    <- getwd()
-engine.path  <- paste0(file.path,'/env/browser/vivaldi/Application/vivaldi.exe')
-# engine.path  <- paste0(file.path,'/browser/maxthon/app/maxthon/bin/maxthon.exe')
+engine  <- paste0(file.path,'/env/browser/vivaldi/Application/vivaldi.exe')
+engine.path  <- paste0('"',file.path,'/env/browser/vivaldi/Application/vivaldi.exe','"')
+
 
 
 system(command = engine.path,invisible = T,wait=F)
 
-runApp('./src/r/app/',launch.browser = function(appUrl){
-        shell(
-          sprintf(
-            '"%s" --app=%s',
-            file.path(engine.path)
-            ,appUrl))})
+ip <- '127.0.0.1'
+port <- '7272'
+ip.port <- paste0('http://',ip,':',port)
+
+runapp.cmd <- paste0(engine.path,' --app=',ip.port)
+
+runApp('./src/r/app/',host = ip,port=7272,launch.browser = shell(runapp.cmd))
