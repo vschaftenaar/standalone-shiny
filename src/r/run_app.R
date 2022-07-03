fde8c4f4fc61f8fefc83008725839ae00d8e97d0
Abulily <4079204+vschaftenaar@users.noreply.github.com>
2022-07-03 09:07:49 +0200

    Add files via upload

--- src/r/run_app.R
+++ src/r/run_app.R
@@ -4,21 +4,20 @@ library(shiny)
 setwd('../../')
 
 file.path    <- getwd()
-engine  <- paste0(file.path,'/env/browser/Application/vivaldi.exe')
-engine.path  <- paste0('"',file.path,'/env/browser/Application/vivaldi.exe','"')
+browser  <- paste0(file.path,'/env/browser/Application/vivaldi.exe')
+browser.path  <- paste0('"',file.path,'/env/browser/Application/vivaldi.exe','"')
 kill.init <- paste0('"',getwd(),'/env/kill_init.bat','"')
 
 system(command = kill.init,invisible = T,wait=T)
-system(command = engine.path,invisible = T,wait=F)
+system(command = browser.path,invisible = T,wait=F)
 
 ip <- '127.0.0.1'
 port <- '7272'
 ip.port <- paste0('http://',ip,':',port)
 
-runapp.cmd <- paste0(engine.path,' --app=',ip.port)
-
-# runApp('./src/r/app/',host = ip,port=7272,launch.browser = shell(runapp.cmd))
+runapp.cmd <- paste0(browser.path,' --app=',ip.port)
 runApp('./src/r/app/',host = ip,port=7272,launch.browser = system(runapp.cmd))
+
 
 Sys.sleep(3)
 system(command = kill.init,invisible = T,wait=T)
